740ac5e8bd3f5bb8a5673223da6955985b3823f8
Dustin <daubrey@gmail.com>
2019-03-12 16:14:16 -0600

    github url

--- install/install_brcd.sh
+++ install/install_brcd.sh
@@ -53,7 +53,9 @@ EOF
 
 }
 
-
+curl -s https://github.com/targaryen/bashrcd/blob/master/aliases/0010--ebrc -o ${BRCDIR}/0010--ebrc
+curl -s https://github.com/targaryen/bashrcd/blob/master/aliases/0010--lbrc -o ${BRCDIR}/0010--lbrc
+curl -s https://github.com/targaryen/bashrcd/blob/master/aliases/0010--rbrc -o ${BRCDIR}/0010--rbrc
 
 check_bashrcd_installed
 
