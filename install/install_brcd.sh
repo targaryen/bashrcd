e38b9a660f1d99804d2af351e6aadedcac39b1bd
Dustin <daubrey@gmail.com>
2019-05-05 16:22:12 -0600

    friendlier install

--- install/install_brcd.sh
+++ install/install_brcd.sh
@@ -6,12 +6,15 @@ BRCDIR=${BRCDIR:-~/.bashrcd}
 
 BACKUPFILE=${BACKUPFILE:-${BACKUPDIR}/bashrcd/bashrc.$(date +%Y%m%d%H%S)}
 
+bold=$(tput bold) # This could also be a color.
+reset=$(tput sgr0)
+
 mkdir -p ${BACKUPDIR}/bashrcd
 cp ${BASHRCFILE} ${BACKUPFILE}
 
 check_bashrcd_installed(){
   echo
-  echo "Checking if bashrcd is already installed to ${BASHRCFILE}"
+  echo "Checking if bashrcd is already installed to ${bold}${BASHRCFILE}${reset}"
 
 
 
@@ -20,13 +23,13 @@ check_bashrcd_installed(){
       echo
       echo "bashrcd has already been added to this system, aborting.."
       echo
-      echo "These were the matching lines in ${BASHRCFILE} :"
+      echo "These were the matching lines in ${bold}${BASHRCFILE}${reset} :"
       echo
       sed -n '/BRCDIR START/,/BRCDIR END/ p' ${BASHRCFILE}
       echo
       echo "You can run remove_bashrcd.sh to remove the above if local, or run from GitHub, eg:"
       echo
-      echo     "curl -s https://raw.githubusercontent.com/targaryen/bashrcd/master/install/remove_brcd.sh | bash"
+      echo     "${bold} curl -s https://raw.githubusercontent.com/targaryen/bashrcd/master/install/remove_brcd.sh | bash ${reset}"
       echo
       echo "Aborting..."
       echo
@@ -71,10 +74,36 @@ echo "Install finished, added the following lines to your ${BASHRCFILE}  : "
 sed -n '/BRCDIR START/,/BRCDIR END/ p' ${BASHRCFILE}
 
 echo
-echo "Your original file is backed up at ${BACKUPFILE}"
+echo "Your original bashrc file is backed up at ${BACKUPFILE}"
 echo
-echo "You can reload your ${BASHRCFILE} with the following"
+echo "You can now use the new aliases ${bold}ebrc${reset}, ${bold}lbrc${reset}, and ${bold}rbrc${reset}.  Usage:"
 echo
-echo "    . ${BASHRCFILE}"
+echo "  ${bold}ebrc${reset} [load order] ${bold}<bashrc entry name>${reset}  "
 echo
+echo "         Edits existing bashrc entry or creates a new one."
+echo
+echo "         The ${bold}load order${reset} is ${bold}optional${reset}.  Use this when you need to finely control load order,"
+echo "         though it is generally better to make each bashrc entry idempotent.  Default is 100, smaller numbers are"
+echo "         loaded first.  If use a value other than 100 then you should use it each edit."
+echo
+echo "  ${bold}lbrc${reset} [load order] ${bold}<bashrc entry name>${reset}  "
+echo
+echo "         Lists the bashrc entries and their load orders"
+echo
+echo "  ${bold}rbrc${reset} [load order] ${bold}<bashrc entry name>${reset}  "
+echo
+echo "         Removes existing bashrc entry."
+echo
+echo "         The ${bold}load order${reset} parameter is only necessary if using non-default load order (any number besides 100). "
+echo "         You don't have to zero pad the load order number. Use ${bold}lbrc${reset} to see the load order and entry names."
+echo
+echo
+echo "Reload your bashrc for the above aliases to take effect immediately (after which, it will automatically be reloaded "
+echo "for you when you create/edit/remove a bashrc entry via the aliases):"
+echo
+echo "${bold}    . ${BASHRCFILE}${reset}"
+echo
+echo "Try ${bold}lbrc${reset} command to see what the above looks like.  Enjoy!"
+echo
+
 
